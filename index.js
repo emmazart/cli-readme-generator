0b384cb81b9187d66b0fec0479f28393d4eceb87
Emma Olson <emma.olson794@gmail.com>
2022-04-25 10:48:52 -0500

    installation of inquirer working locally

--- index.js
+++ index.js
@@ -1,13 +1,82 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt([
+    {
+        name: 'Title',
+        message: 'What is your project title?',
+        type: 'input'
+    },
+    {
+        name: 'Description',
+        message: 'Please enter a project description:',
+        type: 'input'
+    },
+    {
+        name: 'Instructions',
+        message: 'What are the installation instructions?',
+        type: 'input'
+    },
+    {
+        name: 'Useage',
+        message: 'What is the useage information?',
+        type: 'input'
+    },
+    {
+        name: 'Contribution',
+        message: 'What are the contribution guidelines?',
+        type: 'input'
+    },
+    {
+        name: 'Test',
+        message: 'What are the testing instructions?',
+        type: 'input'
+    }
+])
+.then(function(answer){
+    console.log(answer);
+});
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create an array of questions for user input
+// const questions = [
+//     {
+//         name: 'Project Title',
+//         message: 'What is your project title?',
+//         type: 'input'
+//     },
+//     {
+//         name: 'Description',
+//         message: 'Please enter a project description:',
+//         type: 'input'
+//     },
+//     {
+//         name: 'Instructions',
+//         message: 'What are the installation instructions?',
+//         type: 'input'
+//     },
+//     {
+//         name: 'Useage',
+//         message: 'What is the useage information?',
+//         type: 'input'
+//     },
+//     {
+//         name: 'Contribution Guidelines',
+//         message: 'What are the contribution guidelines?',
+//         type: 'input'
+//     },
+//     {
+//         name: 'Test',
+//         message: 'What are the testing instructions?',
+//         type: 'input'
+//     }
+// ];
 
-// Function call to initialize app
-init();
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
